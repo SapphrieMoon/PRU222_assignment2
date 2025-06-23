78ea69a4f6ea3daf9f35a7746330a6a14c48a729
ThienNgoHuong <thiennhse183982@fpt.edu.vn>
2025-06-23 10:33:07 +0700

    add Tag DTO

---
+++ BLL/DTOs/TagDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class TagDTO
+    {
+        public class CreateTagDTO
+        {
+            public required string TagName { get; set; }
+            public required string Note { get; set; }
+        }
+
+        public class UpdateTagDTO
+        {
+            public string? TagName { get; set; }
+            public string? Note { get; set; }
+        }
+
+        public class TagResponseDTO
+        {
+            public int TagId { get; set; }
+            public required string TagName { get; set; }
+            public required string Note { get; set; }
+        }
+    }
+}
---
+++ BLL/Services/TagService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using static BLL.DTOs.TagDTO;
+using DAL.Interfaces;
+
+namespace BLL.Services
+{
+   
+}
