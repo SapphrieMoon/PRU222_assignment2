f255b90692f29d33bccc03629b7145b1dd5f5951
ThienNgoHuong <thiennhse183982@fpt.edu.vn>
2025-06-23 10:16:11 +0700

    create Repo for tag and article

---
+++ DAL/Interfaces/INewsArticleRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface INewsArticleRepository
+    {
+        // CRUD operations for NewsArticle
+        Task<IEnumerable<NewsArticle>> GetAllNewsArticlesAsync();
+        Task<NewsArticle?> GetNewsArticleByIdAsync(int id);
+        Task AddNewsArticleAsync(NewsArticle newsArticle);
+        Task UpdateNewsArticleAsync(NewsArticle newsArticle);
+        Task DeleteNewsArticleAsync(int id);
+
+        // Optional: Check if a news article exists
+        Task<bool> NewsArticleExistsAsync(int id);
+        // Optional: Get news articles by category ID
+        Task<IEnumerable<NewsArticle>> GetNewsArticlesByCategoryIdAsync(int categoryId);
+        // Optional: Get news articles by title
+        Task<IEnumerable<NewsArticle>> GetNewsArticlesByTitleAsync(string title);
+        // Optional: Get news articles by tag ID
+        Task<IEnumerable<NewsArticle>> GetNewsArticlesByTagIdAsync(int tagId);
+
+
+    }
+}
---
+++ DAL/Interfaces/ITagRepository.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ITag
+    {
+        Task<IEnumerable<Tag>> GetAllAsyncs();
+        Task<Tag?> GetByIdAsync(int id);
+        Task AddAsync(Tag tag);
+        Task UpdateAsync(Tag tag);
+        Task DeleteAsync(int id);
+        // Optional: Check if a tag exists
+        Task<bool> ExistsAsync(int id);
+        // Optional: Get tags by news article ID
+        Task<IEnumerable<Tag>> GetTagsByNewsArticleIdAsync(int newsArticleId);
+        // Optional: Get tags by name
+        Task<IEnumerable<Tag>> GetTagsByNameAsync(string name);
+
+    }
+}
