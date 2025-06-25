ec8976d5822d8056a088a375b55b40638f6f9c53
SapphrieMoon <caidenlam@gmail.com>
2025-06-25 23:06:53 +0700

    manage account for admin 1

--- Assignment2/wwwroot/js/_AccountModal.js
+++
@@ -1,9 +0,0 @@
-﻿function openModal(url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('accountModalContent').innerHTML = html;
-            const modal = new bootstrap.Modal(document.getElementById('accountModal'));
-            modal.show();
-        });
-}
