4c9b888ee40d40d970346ce6f6175f662910e75a
AnPTSE183963 <AnPTSE183963@fpt.edu.vn>
2025-06-25 19:10:55 +0700

    Edit Home css

--- Assignment2/Pages/Index.cshtml.cs
+++ Assignment2/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BLL.Interfaces;
+using BLL.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Assignment2.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly INewsArticleService _newsArticleService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<NewsArticleDTO> ActiveArticles { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, INewsArticleService newsArticleService)
         {
             _logger = logger;
+            _newsArticleService = newsArticleService;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            ActiveArticles = await _newsArticleService.GetAllNewsArticlesActiveAsync();
         }
     }
 }
